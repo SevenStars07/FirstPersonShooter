cd8ccbca9238ca6bb857e1a2d827bcc020fc5912
Dragos Adrian <justsevenstars07@gmail.com>
2022-04-20 00:32:00 +0300

    initial commit

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class PlayerMove : MonoBehaviour
+    {
+        public float Speed = 12f;
+        public float JumpHeight = 3f;
+        
+        public Transform GroundCheck;
+        public float GroundDistance = 0.4f;
+        public LayerMask GroundMask;
+        
+        private CharacterController _controller;
+        private Vector3 _velocity;
+        private bool _isGrounded = true;
+    
+        // Start is called before the first frame update
+        void Start()
+        {
+            _controller = GetComponent<CharacterController>();
+
+            if (_controller is null)
+            {
+                Debug.LogError("PlayerMove: No CharacterController found!");
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            _isGrounded = Physics.CheckSphere(GroundCheck.position, GroundDistance, GroundMask);
+                
+            if (_isGrounded && _velocity.y < 0)
+            {
+                _velocity.y = -2f;
+            }
+        
+            var horizontal = Input.GetAxis("Horizontal");
+            var vertical = Input.GetAxis("Vertical");
+        
+            var direction = transform.right * horizontal + transform.forward * vertical;
+        
+            _controller.Move(direction * Speed * Time.deltaTime);
+        
+            if (Input.GetButtonDown("Jump") && _isGrounded)
+            {
+                _velocity.y = Mathf.Sqrt(JumpHeight * -2f * Physics.gravity.y);
+            }
+        
+            _velocity.y += Physics.gravity.y * Time.deltaTime;
+        
+            _controller.Move(_velocity * Time.deltaTime);
+        }
+    }
+}
